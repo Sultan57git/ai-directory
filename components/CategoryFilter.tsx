5764215abde3e2998988932a94498f55cccd50b0
md sultan mahamud <sultanpharu23@gmail.com>
2025-09-05 22:22:19 +0600

    Create CategoryFilter.tsx

---
+++ components/CategoryFilter.tsx
@@ -0,0 +1,17 @@
+// components/CategoryFilter.tsx
+import CATEGORIES from "@/lib/categories";
+
+export default function CategoryFilter() {
+  return (
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-6 gap-3">
+      {CATEGORIES.map((c) => (
+        <button
+          key={c.slug}
+          className="px-3 py-2 rounded-xl border bg-white/50 hover:bg-white transition"
+        >
+          {c.name}
+        </button>
+      ))}
+    </div>
+  );
+}
